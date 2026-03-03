b3722dc8072e376ccb83c99e273e8eed4406320d
Samuel HENRI <159037914+shenri52@users.noreply.github.com>
2026-03-03 16:04:09 +0100

    Update and rename parametres.py to aide.py

---
+++ aide.py
@@ -0,0 +1,3 @@
+    # --- MESSAGE D'INFORMATION ---
+    st.info("💡 Les modifications (ajout, édition, suppression) ne sont pas instantanées. "
+            "Un délai de quelques secondes à une minute peut être nécessaire pour que GitHub mette à jour les données.")
