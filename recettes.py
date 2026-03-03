16c19c2eb28c3a6be9c4e488732729d4ee4b6647
Samuel HENRI <159037914+shenri52@users.noreply.github.com>
2026-03-03 16:03:46 +0100

    Update recettes.py

--- recettes.py
+++ recettes.py
@@ -40,10 +40,6 @@ def supprimer_fichier_github(chemin):
 def afficher():
     st.header("📚 Mes recettes")
 
-    # --- MESSAGE D'INFORMATION ---
-    st.info("💡 Les modifications (ajout, édition, suppression) ne sont pas instantanées. "
-            "Un délai de quelques secondes à une minute peut être nécessaire pour que GitHub mette à jour les données.")
-
     conf = config_github()
     
     # --- CHARGEMENT DYNAMIQUE ---
