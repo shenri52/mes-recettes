e41180cba90877fe6a304290437ae0c3386e66fd
Samuel HENRI <159037914+shenri52@users.noreply.github.com>
2026-03-13 13:11:51 +0100

    Update planning.py

--- planning.py
+++ planning.py
@@ -175,9 +175,10 @@ def afficher():
         st.markdown("<div style='margin-bottom:15px;'></div>", unsafe_allow_html=True)
         
     st.divider()
-    
+       
     if st.button("💾 Enregistrer Planning", use_container_width=True):
         st.session_state.planning_data.update(temp)
+        # On enregistre la totalité des données sans filtre de date
         if sauvegarder_github("data/planning.json", st.session_state.planning_data):
             st.success("Planning enregistré ! 💾")
             time.sleep(1)
