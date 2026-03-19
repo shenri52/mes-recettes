32af493a9c91fd6e0aea58f2b5c22ee6153083d6
Samuel HENRI <159037914+shenri52@users.noreply.github.com>
2026-03-19 20:30:13 +0100

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import requests
+import json
+import base64
+import time
+import datetime
+
+# --- 1. CONFIGURATION ---
+def get_github_config():
+    """Centralise les secrets GitHub pour éviter de les répéter partout."""
+    return {
+        "token": st.secrets["GITHUB_TOKEN"],
+        "owner": st.secrets["REPO_OWNER"],
+        "repo": st.secrets["REPO_NAME"],
+        "headers": {
+            "Authorization": f"token {st.secrets['GITHUB_TOKEN']}",
+            "Accept": "application/vnd.github.v3+json"
+        }
+    }
