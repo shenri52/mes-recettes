4e5230b8146d24a2fafc7b938797b7a290fcd5fb
Samuel HENRI <159037914+shenri52@users.noreply.github.com>
2026-03-20 13:54:18 +0100

    Update stats.py

--- stats.py
+++ stats.py
@@ -3,19 +3,26 @@ import time
 from utils import charger_json_github, actualiser_toutes_les_stats
 
 def afficher():
+    # --- DONNÉES ---
+    data_s = charger_json_github("data/data_stockage.json")
+    stats_r = charger_json_github("data/stats_recettes.json")
+
+    # On extrait la date (si le fichier existe)
+    derniere_maj = data_s.get('derniere_maj', 'Inconnue') if data_s else "Aucune"
+    
     # 1. ACTUALISATION
-    if st.button("🔄 Actualiser les données", use_container_width=True, type="primary"):
+    if st.button("🔄 Actualiser les données", use_container_width=True):
         with st.spinner("Synchronisation globale en cours... ⏳"):
             if actualiser_toutes_les_stats():
                 st.success("Toutes les statistiques sont à jour !")
                 time.sleep(0.5)
                 st.rerun()
+                
+    st.caption(f"🕒 Dernière synchronisation : **{derniere_maj}**")
 
     # --- SECTION RECETTES ---
     st.header("📊 Statistiques")
     st.divider()
-
-    stats_r = charger_json_github("data/stats_recettes.json")
     
     if stats_r:
         st.info(f"**Nombre total de recettes :** {stats_r['total_recettes']}")
@@ -36,7 +43,6 @@ def afficher():
     # --- SECTION STOCKAGE ---
     st.subheader("💾 Stockage")
     
-    data_s = charger_json_github("data/data_stockage.json")
     if data_s:
         st.info(f"**Poids total du dépôt :** {data_s['poids_total_mo']} Mo")
         
@@ -51,8 +57,5 @@ def afficher():
             use_container_width=True, 
             hide_index=True
         )
-        
-        # La date de mise à jour tout en bas
-        st.caption(f"🕒 Dernière synchronisation : **{data_s['derniere_maj']}**")
     else:
         st.warning("⚠️ Aucun relevé de stockage trouvé.")
