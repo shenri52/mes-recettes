5d968551e530ee2dcc764962213ed2dff5c268d6
Samuel HENRI <159037914+shenri52@users.noreply.github.com>
2026-03-23 08:12:36 +0100

    Update coursesaisir.py

--- coursesaisir.py
+++ coursesaisir.py
@@ -94,7 +94,7 @@ def afficher():
     if "reset_count" not in st.session_state:
         st.session_state.reset_count = 0
 
-    st.subheader("📝 Préparer les courses")
+    st.divider()
 
     # --- LES 12 ONGLETS (Retour à Z1, Z2... avec style bouton) ---
     onglets = st.tabs([f"{i+1}" for i in range(12)])
