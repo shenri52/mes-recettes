a8efc2a3646e5af6e256855308837c31a4b0ce9d
Samuel HENRI <159037914+shenri52@users.noreply.github.com>
2026-03-23 11:08:06 +0100

    Update maintenance.py

--- maintenance.py
+++ maintenance.py
@@ -14,11 +14,6 @@ def envoyer_donnees(chemin, contenu, message, est_image=False):
     payload = {"message": message, "content": contenu_b64, "branch": "main"}
     if sha: payload["sha"] = sha
     return requests.put(url, headers=conf['headers'], json=payload).status_code in [200, 201]
-
-def config_github_maintenance():
-    conf = config_github()  # récupère l'existant
-    conf['base_url'] = f"https://api.github.com/repos/{st.secrets['REPO_OWNER']}/{st.secrets['REPO_NAME']}/contents/"
-    return conf
     
 # --- INTERFACE DE MAINTENANCE ---
 def afficher():
