3b8f295861e83c314dbb66a660eeaaf4cbd40e8c
Samuel HENRI <159037914+shenri52@users.noreply.github.com>
2026-03-23 09:43:13 +0100

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,13 @@
+impoimport streamlit as st
+
+# --- 1. CONFIGURATION ---
+def config_github():
+    return {
+        "token": st.secrets["GITHUB_TOKEN"],
+        "owner": st.secrets["REPO_OWNER"],
+        "repo": st.secrets["REPO_NAME"],
+        "headers": {
+            "Authorization": f"token {st.secrets['GITHUB_TOKEN']}",
+            "Accept": "application/vnd.github.v3+json"
+        }
+    }
