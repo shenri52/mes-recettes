7aa515859480dc68778b68a217a0b039cf5ec308
Samuel HENRI <159037914+shenri52@users.noreply.github.com>
2026-03-23 11:13:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,8 +12,6 @@ def verifier_mot_de_passe():
 
     if not st.session_state["authentifie"] and not st.session_state["mode_public"]:
         st.set_page_config(page_title="Mesrecettes", page_icon="🍳", layout="centered")
-        st.markdown("<h2 style='text-align: center;'>🍳 Mes recettes</h2>", unsafe_allow_html=True)
-        st.divider()
         st.markdown("<h3 style='text-align: center;'>🔑 Accès réservé</h3>", unsafe_allow_html=True)
         
         def valider():
