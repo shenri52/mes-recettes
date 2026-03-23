62f36990f955df95afbbf6946383121691dc2b3f
Samuel HENRI <159037914+shenri52@users.noreply.github.com>
2026-03-23 09:59:59 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-# --- 1. CONFIGURATION ---
 def config_github():
     return {
         "token": st.secrets["GITHUB_TOKEN"],
@@ -11,3 +10,19 @@ def config_github():
             "Accept": "application/vnd.github.v3+json"
         }
     }
+
+def envoyer_vers_github(chemin, contenu, message, binaire=False):
+    try:
+        conf = config_github()
+        url = f"https://api.github.com/repos/{conf['owner']}/{conf['repo']}/contents/{chemin}"
+        res_get = requests.get(f"{url}?t={int(time.time())}", headers=conf['headers'])
+        sha = res_get.json().get('sha') if res_get.status_code == 200 else None
+        contenu_b64 = base64.b64encode(contenu if binaire else contenu.encode('utf-8')).decode('utf-8')
+        data = {"message": message, "content": contenu_b64, "branch": "main"}
+        if sha: data["sha"] = sha
+        res = requests.put(url, headers=conf['headers'], json=data)
+        return res.status_code in [200, 201]
+    except Exception as e:
+        import streamlit as st
+        st.error(f"Erreur technique : {str(e)}")
+        return False
