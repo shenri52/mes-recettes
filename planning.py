d26dcd93adbef935273a6baa76557679adc9bde2
Samuel HENRI <159037914+shenri52@users.noreply.github.com>
2026-03-23 08:14:56 +0100

    Update planning.py

--- planning.py
+++ planning.py
@@ -141,6 +141,7 @@ def afficher():
 
     # Bouton retour simplifié
     st.button("⬅️ Retour à l'accueil", use_container_width=True, on_click=aller_accueil)
+    st.divider()
     
     for key, default in {
         'index_complet': charger_donnees("data/index_recettes.json"),
