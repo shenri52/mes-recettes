e4a4823ce6492cd10a396529630824027ae602f2
Samuel HENRI <159037914+shenri52@users.noreply.github.com>
2026-03-25 08:18:46 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -10,6 +10,22 @@ def config_github():
       "repo": st.secrets["REPO_NAME"]
     }
 
+
+def envoyer_vers_github(chemin, contenu, message, est_binaire=False):
+    try:
+        conf = config_github()
+        url = f"https://api.github.com/repos/{conf['owner']}/{conf['repo']}/contents/{chemin}"
+        res_get = requests.get(f"{url}?t={int(time.time())}", headers=conf['headers'])
+        sha = res_get.json().get('sha') if res_get.status_code == 200 else None
+        contenu_b64 = base64.b64encode(contenu if est_binaire else contenu.encode('utf-8')).decode('utf-8')
+        data = {"message": message, "content": contenu_b64, "branch": "main"}
+        if sha: data["sha"] = sha
+        res = requests.put(url, headers=conf['headers'], json=data)
+        return res.status_code in [200, 201]
+    except Exception as e:
+        st.error(f"Erreur technique : {str(e)}")
+        return False
+
 # --- GESTION DE L'INDEX DES RECETTES---
 def charger_index():
     conf = config_github()
