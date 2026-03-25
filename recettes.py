24bdfe912c5f09501890cda9cab428663a2c082e
Samuel HENRI <159037914+shenri52@users.noreply.github.com>
2026-03-25 08:18:31 +0100

    Update recettes.py

--- recettes.py
+++ recettes.py
@@ -1,22 +1,7 @@
 import streamlit as st
 import requests, json, base64, time, uuid, io
 from PIL import Image
-from utils import config_github, charger_index, sauvegarder_index, verifier_doublon
-
-def envoyer_vers_github(chemin, contenu, message, est_binaire=False):
-    try:
-        conf = config_github()
-        url = f"https://api.github.com/repos/{conf['owner']}/{conf['repo']}/contents/{chemin}"
-        res_get = requests.get(f"{url}?t={int(time.time())}", headers=conf['headers'])
-        sha = res_get.json().get('sha') if res_get.status_code == 200 else None
-        contenu_b64 = base64.b64encode(contenu if est_binaire else contenu.encode('utf-8')).decode('utf-8')
-        data = {"message": message, "content": contenu_b64, "branch": "main"}
-        if sha: data["sha"] = sha
-        res = requests.put(url, headers=conf['headers'], json=data)
-        return res.status_code in [200, 201]
-    except Exception as e:
-        st.error(f"Erreur technique : {str(e)}")
-        return False
+from utils import config_github, charger_index, sauvegarder_index, verifier_doublon, envoyer_vers_github
 
 def supprimer_fichier_github(chemin):
     conf = config_github()
