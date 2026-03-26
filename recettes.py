c2a2caccdd3efe9fc5d874fc8794060626728d4d
Samuel HENRI <159037914+shenri52@users.noreply.github.com>
2026-03-26 19:37:57 +0100

    Update recettes.py

--- recettes.py
+++ recettes.py
@@ -59,10 +59,9 @@ def afficher():
     
     # On récupère la valeur stockée (qui peut venir de l'URL via app.py)
     valeur_actuelle = st.session_state.get("select_recette", "---")
-    try:
-        idx_depart = options.index(valeur_actuelle)
-    except ValueError:
-        idx_depart = 0
+    
+    if valeur_actuelle not in options:
+        valeur_actuelle = "---"
     
     choix = st.selectbox(
         "Sélectionner une recette", 
