5c1fdf4a1ab7dc766aa811b731885abed0ed0772
Samuel HENRI <159037914+shenri52@users.noreply.github.com>
2026-03-27 06:54:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -66,6 +66,7 @@ def aller_accueil():
     st.query_params.clear()
     st.session_state.page = 'accueil'
     st.session_state["mode_public"] = False
+    st.session_state["select_recette"] = "---"
 
 # --- EXÉCUTION DE L'APPLICATION ---
 if verifier_mot_de_passe():
